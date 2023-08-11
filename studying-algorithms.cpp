40e534a65debb915b583c649de64d1a04c40f487
Luaneo <91953084+Luaneo@users.noreply.github.com>
2023-08-11 23:52:53 +0300

    greedy algorithms with sorting - very easy

---
+++ studying-algorithms.cpp
@@ -0,0 +1,31 @@
+// studying-algorithms.cpp
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n, x;
+	cin >> n >> x;
+
+	vector<int> a;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	sort(a.begin(), a.end());
+
+	int res = 0;
+	for (int e : a) {
+		if (x >= e) {
+			res++;
+			x -= e;
+		}
+	}
+
+	cout << res;
+	return 0;
+}
